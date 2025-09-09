1303b42f744312cab39ed0c7cd94d5f6af6d7fbf
osservavi <dasavas70@gmail.com>
2025-09-09 17:20:31 +0300

    feat: add initial version of HH.ru data parser

---
+++ src/parsing_data.py
@@ -0,0 +1,80 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+from datetime import datetime
+import os
+
+def get_vacancy_links(query='Аналитик данных', pages=1):
+    '''
+    Формирует список ссылок на вакансии в результате поиска на HH.ru
+    '''
+    base_url = 'https://hh.ru'
+    vacancies_list = list()
+    headers = {'User-Agent': 
+           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36'}
+    
+    for page in range(pages):
+        url = f'{base_url}/search/vacancy?text={query}&experience=noExperience&page={page}'
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        vacancy_cards = soup.find_all('a', class_="magritte-link___b4rEM_6-0-5 magritte-link_mode_primary___l6una_6-0-5 magritte-link_style_neutral___iqoW0_6-0-5 magritte-link_enable-visited___Biyib_6-0-5")
+        for card in vacancy_cards:
+            link = card['href']
+            clean_link = link.split('?')[0]
+            vacancies_list.append(clean_link)
+        
+        time.sleep(3)
+    
+    return vacancies_list
+
+def parse_vacancy_page(url):
+    '''
+    Собирает данные с каждой вакансии из собранного списка get_vacancy_links()
+    '''
+    headers = {'User-Agent': 
+           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    skills = list()
+    skills_info = soup.find('ul', class_='vacancy-skill-list--JsTYRZ5o6dsoavK7')
+    if skills_info:
+        skills = [skill.text.strip() for skill in skills_info.find_all('li')]
+
+    vacancy_data = {
+        'skills': skills
+    }
+
+    return vacancy_data
+
+def main():
+    '''
+    Производит сбор данных и сохраняет их в формате .CSV
+    '''
+    print('Производится сбор ссылок...')
+    links = get_vacancy_links()
+
+    vacancies_data = list()
+    for i, link in enumerate(links):
+        print(f'Парсинг {i}й вакансии из {len(links)}: {link}')
+        try:
+            data = parse_vacancy_page(link)
+            vacancies_data.append(data)
+        except Exception as e:
+            print(f'! Ошибка при парсинге {link}: {e}')
+        
+        time.sleep(2)
+
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    raw_data_filename = f'vacancies_info_{current_date}.csv'
+    raw_data_path = os.path.join('data', 'raw', raw_data_filename)
+    
+    df = pd.DataFrame(vacancies_data)
+    df.to_csv(raw_data_path, index=False, encoding='utf-8-sig')
+    print(f"Данные сохранены в: {raw_data_path}")
+
+
+if __name__ == '__main__':
+    main()
